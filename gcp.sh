ec5148cb9baf6b5a276c289e6d34374578746812
Anson Sarosh Dsouza <62369900+theonlyanson@users.noreply.github.com>
2022-10-04 14:08:14 -0400

    Update gcp.sh

--- gcp.sh
+++ gcp.sh
@@ -1,1 +1,16 @@
 
+
+vnc_password=123456
+vnc_resolution=1280x720
+
+vnc_password=123456
+vnc_resolution=1280x720
+
+vnc_password=123456
+vnc_resolution=1280x720
+
+vnc_password=123456
+vnc_resolution=1280x720
+
+vnc_password=123456
+vnc_resolution=1280x720
